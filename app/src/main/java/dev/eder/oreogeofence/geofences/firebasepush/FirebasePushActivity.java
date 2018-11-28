0a1dfd84372cc32fb0120e52c0198f497f05bed6
Eder Padilla 🌮 <eder.padilla97@gmail.com>
2018-11-27 22:35:07 -0600

    Ready push notifications sample 😬🚀🇲🇽

---
+++ app/src/main/java/dev/eder/oreogeofence/geofences/firebasepush/FirebasePushActivity.java
@@ -0,0 +1,15 @@
+package dev.eder.oreogeofence.geofences.firebasepush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dev.eder.oreogeofence.R;
+
+public class FirebasePushActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_push);
+    }
+}
